5103a97a62884cb06eec87da1e22215098d2bbb5
nikita.mishchenko <nekitoso2@yandex.ru>
2022-04-18 11:02:47 +0300

    Modified Task_009

--- Practice/Task_009.py
+++ Practice/Task_009.py
@@ -1,7 +1,8 @@
 # Показать последнюю цифру трёхзначного числа
-
-number = int(input('Input number: '))
-if(number > 99 and number < 1000):
-    print(number % 10)
-else:
-    print('Number is not three digits')
+while True:
+    number = int(input('Input number: '))
+    if(number > 99) and (number < 1000):
+        print(number % 10)
+        break
+    else:
+        print('Number is not three digits.')
