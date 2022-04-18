51c7897e332b9e48604a262c84eaec1adc7ed2e3
nikita.mishchenko <nekitoso2@yandex.ru>
2022-04-18 11:16:23 +0300

    Modified Task_010

--- Practice/Task_010.py
+++ Practice/Task_010.py
@@ -1,10 +1,10 @@
 # Дано число в отрезке [10, 99].Показать наибольшую цифру числа.
 
 number = int(input('Input number: '))
-if(number > 9 and number < 100):
+if(number > 9) and (number < 100):
     tmp = number % 10
     number = number // 10
-    if(number > tmp):
+    if (number > tmp):
         print(number)
     else:
         print(tmp)
