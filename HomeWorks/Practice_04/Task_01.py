a21f795bc2141eee4427cc91dd7b11938694a244
nikita.mishchenko <nekitoso2@yandex.ru>
2022-05-03 19:27:46 +0400

    Completing Task_01

--- HomeWorks/Practice_04/Task_01.py
+++ HomeWorks/Practice_04/Task_01.py
@@ -2,3 +2,18 @@
 # Пример:
 #
 # при $d = 0.001, π = 3.141.$ $10^{-1} ≤ d ≤10^{-10}$
+
+import math
+
+def Accuracy(data):
+    i = 0
+    while(1 > data):
+        i += 1
+        data = data * 10
+    return i
+
+def AccuracyPI(precision):
+    return round(math.pi, precision)
+
+precision = float(input('Input precision (example: 0.001): '))
+print(f'{AccuracyPI(Accuracy(precision))}')
