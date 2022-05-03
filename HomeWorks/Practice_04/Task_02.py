ee9d797500b18a3495c32c06d4f0ce9de78ebdb8
nikita.mishchenko <nekitoso2@yandex.ru>
2022-05-03 20:07:16 +0400

    Completing Task_02

--- HomeWorks/Practice_04/Task_02.py
+++ HomeWorks/Practice_04/Task_02.py
@@ -1,1 +1,16 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+def factorization(n):
+   i = 2
+   primfac = []
+   while i * i <= n:
+       while n % i == 0:
+           primfac.append(i)
+           n = n / i
+       i = i + 1
+   if n > 1:
+       primfac.append(n)
+   return primfac
+
+number = int(input('Inpunt number: '))
+print(f'{factorization(number)}')
