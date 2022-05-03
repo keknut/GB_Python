b6063a2c1e2e09579f4c0efac734612fbf1123d6
nikita.mishchenko <nekitoso2@yandex.ru>
2022-05-03 21:57:57 +0400

    working homework task_03

--- HomeWorks/Practice_04/Task_03.py
+++ HomeWorks/Practice_04/Task_03.py
@@ -1,2 +1,21 @@
 # Задайте последовательность чисел. Напишите программу,
 # которая выведет список неповторяющихся элементов исходной последовательности.
+
+string = input('Input array: ')
+array = string.split(' ')
+dictionary = {0 : 0}
+flag = True
+
+for i in range (0, len(array)):
+    flag = True
+    for j in dictionary.keys():
+        if(j == array[i]):
+            dictionary[j]+=1
+            flag = False
+            continue
+    if flag:
+        dictionary[i] = 1
+
+for i in dictionary.keys():
+    if (dictionary[i] == 1):
+        print(f'{i}', end=' ')
--- Lections/Tests.py
+++ Lections/Tests.py
@@ -1,14 +1,42 @@
-dictionary = {  'up':'up',
-                'left':'left',
-                'right':'right',
-                'bottom':'bottom'   
-}
-path = "test.txt"
+# dictionary = {'up': 'up',
+#               'left': 'left',
+#               'right': 'right',
+#               'bottom': 'bottom'
+#               }
+# path = "test.txt"
 
-with open(path, 'w') as dataWrite:
-    for i in dictionary.items():
-        dataWrite.write(f'{i}\n')
+# with open(path, 'w') as dataWrite:
+#     for i in dictionary.items():
+#         dataWrite.write(f'{i}\n')
 
-with open(path, 'r') as dataRead:
-    allData = dataRead.read()
-print(allData)
+# with open(path, 'r') as dataRead:
+#     allData = dataRead.read()
+# print(allData)
+
+
+# dictionary = {
+#     1 : 0,
+#     2 : 0,
+#     3 : 0,
+#     4 : 0, 
+#     5 : 0,
+#     6 : 0
+# }
+
+# array = [1, 2, 1, 3, 4, 5, 5, 6, 5, ]
+
+# for i in range (0, len(array)):
+#     for j in dictionary.keys():
+#         if(j == array[i]):
+#             dictionary[j]+=1
+# for i in dictionary.keys():
+#     if(dictionary[i] == 1):
+#         print(i, end=' ')
+
+
+dictionary = {}
+numbers = [1, 2, 4, 6, 2]
+
+dictionary[numbers[0]]+= 1
+
+print(dictionary)
