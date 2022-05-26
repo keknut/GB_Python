573f348c6c93a45d66117dfb6b69b5daa09e75d3
nikita.mishchenko <nekitoso2@yandex.ru>
2022-05-26 22:03:04 +0400

    completing task_01. modified task_02

--- HomeWorks/Practice_05/Task_02.py
+++ HomeWorks/Practice_05/Task_02.py
@@ -7,3 +7,51 @@
 # a) Добавьте игру против бота
 #
 # b) Подумайте как наделить бота "интеллектом"
+
+# from tkinter import *
+
+# def clicked():
+#     lbl.configure(text='Я же просил!')
+
+# window = Tk()
+# window.title('Игра в конфеты')
+# window.geometry('500x500')
+
+# lbl = Label(window, text="Привет")  
+# lbl.grid(column=0, row=0)
+
+# btn = Button(window, text="Не нажимать!", command=clicked)
+# btn.grid(column=0, row=1)
+
+# window.mainloop()
+
+import random
+
+candy_sum = 2021
+player_list = ['player 1', 'player 2']
+first_player = random.choice(player_list)
+player_turn = 0
+flag = True
+
+print(f'{player_list[0]} vs {player_list[1]}\nFIGHT!\n')
+
+if first_player == player_list[0]:
+    second_player = player_list[1]
+else:
+    second_player = player_list[0]
+
+while candy_sum > 0:
+    if flag:
+        player_turn = int(input(f'Candys: {candy_sum}\nTurn: {first_player}\nInput candys: '))
+        flag = False
+        candy_sum -= player_turn
+        print('------')
+        if candy_sum <= 0:
+            print(f'{first_player} win!')
+    else:
+        player_turn = int(input(f'Candys: {candy_sum}\nTurn: {second_player}\nInput candys: '))
+        flag = True
+        candy_sum -= player_turn
+        print('------')
+        if candy_sum <= 0:
+            print(f'{second_player} win!')
---
+++ HomeWorks/Practice_05/Tests.py
@@ -0,0 +1,16 @@
+# my_str = 'Усложнение. В данном решении пользователь сам задаёт строку. Если программа найдёт слова содержащие эту строку, она их удалит.'
+# print(f'Actual string:\n{my_str}\n----')
+# my_str = my_str.split(' ')
+
+# for word in my_str:
+#     if '.' in word:
+
+from operator import index
+
+
+li =['1', '2', '3', '45', '6']
+for word in li:
+    if '5' in word:
+        word.replace('5')
+        li.insert(index, '5')
+print(li)
