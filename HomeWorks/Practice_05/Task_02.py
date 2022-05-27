5ddb689cc809e7fcd1780e054be8f687d62c52db
nikita.mishchenko <nekitoso2@yandex.ru>
2022-05-27 15:20:12 +0400

    add functions in Task_02

--- HomeWorks/Practice_05/Task_02.py
+++ HomeWorks/Practice_05/Task_02.py
@@ -8,37 +8,84 @@
 #
 # b) Подумайте как наделить бота "интеллектом"
 
-# from tkinter import *
-
-# def clicked():
-#     lbl.configure(text='Я же просил!')
-
-# window = Tk()
-# window.title('Игра в конфеты')
-# window.geometry('500x500')
-
-# lbl = Label(window, text="Привет")  
-# lbl.grid(column=0, row=0)
-
-# btn = Button(window, text="Не нажимать!", command=clicked)
-# btn.grid(column=0, row=1)
-
-# window.mainloop()
-
 import random
 
+def main_menu():
+    text = '1. Player vs Player\n2. Player vs Bot\n3. Exit\n'
+    choise = input(f'------\n{text}')
+    return choise
+
+def pvp_names():
+    print('------')
+    first_player = input('Enter the name of the First Player. The default name is Player 1:\n')
+    second_player = input('Enter the name of the Second Player. The default name is Player 2:\n')
+    if first_player != '':
+        player_list[0] = first_player
+    if second_player != '':
+        player_list[1] = second_player
+    return player_list
+
+def pvb_names():
+    bots_name = ['Anna', 'Maria', 'David', 'John', 'Simon', 'Alfred', 'Alex']
+    player_list[1] = random.choices(bots_name)
+
+    first_player = input('Enter the name of the Player. The default name is Player 1:\n')
+    if first_player != '':
+        player_list[0] = first_player
+    return player_list
+
+def bot_ai(candy_sum, max_turn, first_turn):
+    if first_turn:
+        if random.randint(1,9) > 6:
+            return candy_sum % max_turn
+        else:
+            return classic_bot_turn(max_turn)
+    else:
+        return classic_bot_turn(max_turn)
+
+def classic_bot_turn(max_turn):
+    return random.randint(1, max_turn)
+
+def pvp_turns(candy_sum, player):
+    candy_sum -= int(input(f'Candys: {candy_sum}\nTurn: {player}\nInput candys: '))
+    print('------')
+    if candy_sum <= 0:
+        print(f'{player} win!')
+    return candy_sum
+
+def game(candy_sum, player_list):
+    
+
 candy_sum = 2021
-player_list = ['player 1', 'player 2']
-first_player = random.choice(player_list)
+player_list = ['Player 1', 'Player 2']
+
+
 player_turn = 0
 flag = True
+first_bot_turn = False
+loop = True
 
-print(f'{player_list[0]} vs {player_list[1]}\nFIGHT!\n')
+while loop:
+    choice = main_menu()
+    if choice == '1':
+        player_list = pvp_names()
+        first_player = random.choice(player_list)
+        if first_player == player_list[0]:
+            second_player = player_list[1]
+        else:
+            second_player = player_list[0]
+        print(f'{player_list[0]} vs {player_list[1]}\nFIGHT!\n')
+    if choice == '2':
+
+    if choice == '3':
+        loop = False
+
 
 if first_player == player_list[0]:
     second_player = player_list[1]
 else:
     second_player = player_list[0]
+    first_bot_turn = True
 
 while candy_sum > 0:
     if flag:
--- HomeWorks/Practice_05/Tests.py
+++ HomeWorks/Practice_05/Tests.py
@@ -5,12 +5,16 @@
 # for word in my_str:
 #     if '.' in word:
 
-from operator import index
+# from operator import index
 
 
-li =['1', '2', '3', '45', '6']
-for word in li:
-    if '5' in word:
-        word.replace('5')
-        li.insert(index, '5')
-print(li)
+# li =['1', '2', '3', '45', '6']
+# for word in li:
+#     if '5' in word:
+#         word.replace('5')
+#         li.insert(index, '5')
+# print(li)
+
+sum = 2021
+turn = 28
+print(sum % turn)
