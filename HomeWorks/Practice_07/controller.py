3a73e218ed0695dd39d7aa73c8aad602f2037a0f
nikita.mishchenko <nekitoso2@yandex.ru>
2022-06-21 23:01:50 +0400

    Modified HomeWork Practice_07

--- HomeWorks/Practice_07/controller.py
+++ HomeWorks/Practice_07/controller.py
@@ -1,20 +1,38 @@
 import view as v
 import data
 import csv_worker as csvW
+from pathlib import Path
 
 def controller_main():
-    csv_path = 'GB_Python\HomeWorks\Practice_07\phone_book.csv'
+    # Путь к csv файлу
+    
+    csv_path = Path(Path.cwd(), 'phone_book.csv')
+    
+    # Пока переменная True, программа исполняется
     program = True
+    
+    # Выводит на консоль превью 
     v.preview()
+
+    # Здесь попытка открыть файл с базой контактов
+    # Если не находит файл, создаёт новый с тестовым дата-сетом
     if csvW.found_csv(csv_path):
         v.file_found()
         data.set_contacts(csvW.csv_read(csv_path))
     else:
         v.file_not_found()
         csvW.csv_write_contacts(data.get_contacts(), csv_path)
+
+    # Основное тело программы.
     while program:
+        
+    # Выводит на консоль главное меню
         v.main_menu()
+        # Опрашивает пользователя, какой пункт меню он выбирает
         choice = v.main_menu_choice()
+
+        # Пункт меню "Добавить контакт"
+        # Добавляет контакт сразу в файл csv 
         if choice == '1':
             csvW.csv_add(data.add_contact(v.add_contact_first_name(), v.add_contact_last_name(), v.add_contact_phone()), csv_path)
             continue
--- Practice/Tests.py
+++ Practice/Tests.py
@@ -1,44 +1,4 @@
-import csv
-
-def csv_write_contacts(data):
-    with open('GB_Python\Practice\phone_book.csv', 'w') as file:
-        writer = csv.DictWriter(file, fieldnames = list(data[0].keys()), quoting=csv.QUOTE_NONNUMERIC)
-        writer.writeheader()
-        for row in data:
-            writer.writerow(row)
-
-def csv_add(data):
-    with open('GB_Python\Practice\phone_book.csv', 'a') as file:
-        writer = csv.DictWriter(file, fieldnames = list(data[0].keys()), quoting=csv.QUOTE_NONNUMERIC)
-        for row in data:
-            writer.writerow(row)
-
-
-def csv_read():
-    contacts = []
-    with open('GB_Python\Practice\phone_book.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            contacts.append(row)
-    return contacts
-
-contacts = [{   'First name' : 'Jhon',
-                'Last name' : 'Smith',
-                'Phone' : '+79012345678'},
-            {   'First name' : 'Simon',
-                'Last name' : 'Red',
-                'Phone' : '+79012312312'},
-            {   'First name' : 'Samantha',
-                'Last name' : 'Scarlett',
-                'Phone' : '+79'},
-            {   'First name' : 'Jhon',
-                'Last name' : 'Scarlett',
-                'Phone' : '+7987'}]
-
-new_contact = [{   'First name' : 'Jhon',
-                'Last name' : 'Connor',
-                'Phone' : '+7901222222'}]
-
-csv_write_contacts(contacts)
-csv_add(new_contact)
-print(csv_read())
+from pathlib import Path
+# dir_path = Path.cwd()
+# path = Path(dir_path, )
+print(Path.cwd())
